dc7fb740da4ba58971389b53be319dd23a9b5586
yeongun130 <yeongunj625@gmail.com>
2023-02-13 10:26:52 +0900

    commit

---
+++ app/src/main/java/com/example/android/Button_and_Stringsorce.kt
@@ -0,0 +1,11 @@
+package com.example.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Button_and_Stringsorce : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_button_and_stringsorce)
+    }
+}
