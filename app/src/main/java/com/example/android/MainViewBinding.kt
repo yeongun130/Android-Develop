c3346390f62a8f9560f52f4e31f1a4368f8b76bb
정영운 <108732289+yeongun130@users.noreply.github.com>
2023-02-17 11:22:53 +0900

    액티비티 값 주고받기(ViewBinding)

--- app/src/main/java/com/example/android/MainViewBinding.kt
+++ app/src/main/java/com/example/android/MainViewBinding.kt
@@ -3,7 +3,7 @@ package com.example.android
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-//import android.widget.Toast
+import android.widget.Toast
 import com.example.android.databinding.ActivityMainViewBindingBinding
 
 class MainViewBinding : AppCompatActivity() {
@@ -18,21 +18,21 @@ class MainViewBinding : AppCompatActivity() {
             val intent = Intent(this, ServeViewBinding::class.java)
             intent.putExtra("from1","Hello Bundle")
             intent.putExtra("from2",2023)
-            //startActivityForResult(intent,99)
+            startActivityForResult(intent,99)
         }
     }
 
-//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//        super.onActivityResult(requestCode, resultCode, data)
-//
-//        if(resultCode == RESULT_OK) {
-//            when(requestCode) {
-//                99 -> {
-//                    data?.getStringExtra("returnValue").let { message ->
-//                        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-//                    }
-//                }
-//            }
-//        }
-//    }
+   override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+       super.onActivityResult(requestCode, resultCode, data)
+
+       if(resultCode == RESULT_OK) {
+           when(requestCode) {
+               99 -> {
+                   data?.getStringExtra("returnValue").let { message ->
+                       Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+                   }
+               }
+           }
+       }
+   }
 }
