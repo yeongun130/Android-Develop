f4adf95c850fb18a99eaaddcf3208c207c2c2857
정영운 <108732289+yeongun130@users.noreply.github.com>
2023-02-20 12:08:45 +0900

    Fragment 연습

--- app/src/main/java/com/example/android/DetailFragment.kt
+++ app/src/main/java/com/example/android/DetailFragment.kt
@@ -3,9 +3,9 @@ package com.example.android
 import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-//import android.view.LayoutInflater
+import android.view.LayoutInflater
 import android.view.View
-//import android.view.ViewGroup
+import android.view.ViewGroup
 import com.example.android.databinding.FragmentDetailBinding
 
 class DetailFragment : Fragment() {
@@ -18,13 +18,13 @@ class DetailFragment : Fragment() {
         if(context is FragAcitivity) frag = context
     }
 
-//    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        binding = FragmentDetailBinding.inflate(inflater,container,false)
-//        return binding.root
-//    }
+   override fun onCreateView(
+       inflater: LayoutInflater, container: ViewGroup?,
+       savedInstanceState: Bundle?
+   ): View? {
+       binding = FragmentDetailBinding.inflate(inflater,container,false)
+       return binding.root
+   }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
